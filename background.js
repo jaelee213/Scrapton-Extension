18c7af5f462ab8be5df955b0c8aa867820f554b9
jaelee213 <jaelee213@gmail.com>
2019-01-04 20:58:35 -0800

    completed version 0.1.0 of Scrapton extension

--- background.js
+++ background.js
@@ -1,4 +1,3 @@
-
 chrome.browserAction.onClicked.addListener(clickedButton);
 
 function clickedButton(tab) {
--- popup.js
+++ popup.js
@@ -1,5 +1,4 @@
 chrome.runtime.onMessage.addListener(gotMessage);
-
 function gotMessage(message, sender, sendResponse) {
 	if (message.text === "clicked") {
 		window.alert('SCRAPTON TIME');
@@ -7,15 +6,16 @@ function gotMessage(message, sender, sendResponse) {
 		audio = chrome.extension.getURL(audio);
 		audio = new Audio(audio);
 		audio.play();
+		setTimeout(() => {
+			audio.pause();
+		}, 17678);
 		scrapton();
 	}
 }
-
 function scrapton() {
 	let filenames = ["jim.png", "dwight.png", "michael.png", "pam.png", "dwightkevin.png", "kevinmalone.png", "kevinchilli.png", "kevinstare.png", "om1.png", "om2.png", "om3.png", "om4.png", "om5.png"];
 	let soundnames = ["jim.mp3", "dwight.mp3", "michael.mp3", "pam.mp3", "dwightkevin.mp3", "kevinmalone.mp3", "kevinchilli.mp3", "kevinstare.mp3", "om1.mp3", "michael.mp3", "dwight.mp3", "om1.mp3", "om5.mp3"];
 	let imgs = document.querySelectorAll('img');
-	
 	for (imgElt of imgs) {
 		let randomNum = Math.floor(Math.random() * filenames.length);
 		let file = './images/' + filenames[randomNum];
